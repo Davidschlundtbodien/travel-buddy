2b8d2f12d2b1bfbfb67f185ca83b4f7c4bf9335d
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-08-04 10:06:57 -0600

    Add Destination class tests

---
+++ test/Destination-test.js
@@ -0,0 +1,26 @@
+import chai from 'chai';
+const expect = chai.expect;
+import destinations from './sample-data/destinations';
+import Destination from '../src/classes/Destination';
+
+describe('Destination', () => {
+  let destination
+
+  beforeEach(() => {
+    destination = new Destination(destinations[0])
+  });
+
+  it('should be a function', () => {
+    expect(Destination).to.be.a('function');
+  });
+
+  it('should intialize a traveler object', () => {
+    expect(destination).to.be.an.instanceof(Destination)
+    expect(destination.id).to.equal(1)
+    expect(destination.destination).to.equal('Lima, Peru')
+    expect(destination.estimatedLodgingCostPerDay).to.equal(70)
+    expect(destination.estimatedFlightCostPerPerson).to.equal(400)
+    expect(destination.image).to.equal("https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80")
+    expect(destination.alt).to.equal("overview of city buildings with a clear sky")
+  })
+});
--- test/sample-data/destinations.js
+++ test/sample-data/destinations.js
@@ -1,10 +1,11 @@
-export const destinations = [{
-  "id": 1,
-  "destination": "Lima, Peru",
-  "estimatedLodgingCostPerDay": 70,
-  "estimatedFlightCostPerPerson": 400,
-  "image": "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
-  "alt": "overview of city buildings with a clear sky"
+const destinations = [
+  {
+    "id": 1,
+    "destination": "Lima, Peru",
+    "estimatedLodgingCostPerDay": 70,
+    "estimatedFlightCostPerPerson": 400,
+    "image": "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
+    "alt": "overview of city buildings with a clear sky"
   }, {
     "id": 2,
     "destination": "Stockholm, Sweden",
@@ -54,3 +55,5 @@ export const destinations = [{
     "alt": "city during the day time with eiffel tower"
   }
 ];
+
+export default destinations
