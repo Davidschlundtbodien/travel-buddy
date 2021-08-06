087f6d6de50f1be04c64cc1595bb5471edcfb260
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-08-06 16:16:50 -0600

    Add tests to traveler

--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -1,13 +1,21 @@
 import chai from 'chai';
 const expect = chai.expect;
 import travelers from './sample-data/travelers';
+import trips from './sample-data/trips';
+import destinations from './sample-data/destinations';
 import Traveler from '../src/classes/Traveler';
+import Trip from '../src/classes/Trip';
+import TripRepository from '../src/classes/TripRepository'
+import DestinationRepository from '../src/classes/DestinationRepository';
+
 
 describe('Traveler', () => {
-  let traveler
+  let traveler, tripRepo, destinationRepo
 
   beforeEach(() => {
-    traveler = new Traveler(travelers[0])
+    destinationRepo = new DestinationRepository(destinations)
+    tripRepo = new TripRepository(trips)
+    traveler = new Traveler(travelers[0], tripRepo)
   });
 
   it('should be a function', () => {
@@ -19,5 +27,13 @@ describe('Traveler', () => {
     expect(traveler.id).to.equal(1)
     expect(traveler.name).to.equal('Ham Leadbeater')
     expect(traveler.type).to.equal('relaxer')
+  })
+
+  it('should store respective trips', () => {
+    expect(traveler.trips.length).to.equal(2)
+  });
+
+  it('should calculate total cost of all trips', () => {
+    expect(traveler.calculateTotalSpent(destinationRepo)).to.equal(6616.5)
   })
 });
