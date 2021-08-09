3f23428fba1db3771bfd471082b33b1de64ef329
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-08-09 16:23:56 -0600

    Add dom updates for traveler

---
+++ src/domUpdates.js
@@ -0,0 +1,66 @@
+import {destinationRepo, tripRepo, traveler, travelerLogin} from './apiRequests'
+
+let today = '2021/08/11'
+
+const totalSpent = document.getElementById('totalSpent')
+const travelerName = document.getElementById('travelerName')
+const bookTripButton = document.getElementById('bookTripButton')
+const pendingGallery = document.getElementById('pendingGallery')
+const upcomingGallery = document.getElementById('upcomingGallery')
+const pastGallery = document.getElementById('pastGallery')
+
+const updateDom = () => {
+  updateHeader()
+  updateTripGallery(pendingGallery, 'pending');
+  updateTripGallery(upcomingGallery, 'upcoming');
+  updateTripGallery(pastGallery, 'past');
+
+}
+
+setTimeout(() => {
+  updateDom()
+}, 70);
+
+const updateHeader = () => {
+  totalSpent.innerText = `$${traveler.totalSpent}`
+  travelerName.innerText = `${traveler.name}`
+}
+
+const updateTripGallery = (gallery, type) => {
+  gallery.innerHTML = "";
+  switch (type) {
+    case 'pending':
+      let pendingTrips = traveler.trips.filter(trip => trip.status === 'pending')
+      pendingTrips.forEach(trip => {
+        buildTripCard(trip, gallery)
+      });
+      break;
+    case 'upcoming':
+      let upcomingTrips = traveler.trips.filter(trip => trip.status === 'approved' && trip.date >= today)
+      upcomingTrips.forEach(trip => {
+        buildTripCard(trip, gallery)
+      });
+      break;
+    case 'past':
+      let pastTrips = traveler.trips.filter(trip => trip.status === 'approved' && trip.date < today)
+      pastTrips.forEach(trip => {
+        buildTripCard(trip, gallery)
+      });
+      break;
+    default:
+    console.log('no options selected')
+  }
+}
+
+const buildTripCard = (trip, gallery) => {
+  let destination = destinationRepo.findDestination(trip.destinationID)
+
+  gallery.innerHTML += `
+  <article class="trip-card">
+    <img src="${destination.image}" alt="${destination.alt}">
+    <div class="trip-description">
+      <p>${destination.destination}</p>
+      <p>${trip.date}</p>
+    </div>
+  </article>`
+}
